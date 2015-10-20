8fd74b4d27f2c78f7e93ed001fba5fd5f2f5f2cd
Fei Liu <liu.feiwood@gmail.com>
2015-10-21 00:25:56 +0200

    add deploy.sh

---
+++ deplay.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+REPO=/var/www/pdb-fr
+APP=$REPO/application
+SERVER=$REPO/server
+
+echo "========================================================================"
+echo "git pull (update repo)"
+echo "                        => rebuild the React app"
+echo "                                                  => restart node server"
+echo "========================================================================"
+
+cd $REPO && git pull
+cd $APP && webpack
+cd $SERVER && forever restartall
